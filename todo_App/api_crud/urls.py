469586445e36b6c8dc70bbaed74dd58967f6361e
Bragadeesh001 <bragadeeshs75@gmail.com>
2023-07-25 10:45:45 +0530

    major_changes_2

--- todo_App/api_crud/urls.py
+++ todo_App/api_crud/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
 	path('task-detail/', views.taskDetail, name="task-detail"),
 	path('task-create/', views.taskCreate, name="task-create"),
 
-	path('task-update/<str:pk>/', views.taskUpdate, name="task-update"),
+	path('task-update/', views.taskUpdate, name="task-update"),
 	path('task-delete/<str:pk>/', views.taskDelete, name="task-delete"),
-    path('home/', views.home, name='lhome'),
 ]
--- todo_App/api_crud/views.py
+++ todo_App/api_crud/views.py
@@ -19,76 +19,61 @@ def apiOverview(request):
 		'Delete':'/task-delete/<str:pk>/',
 		}
 
-	return Response(api_urls)
+	return render(request, 'home.html', {'data':api_urls})
 
 @api_view(['GET'])
 def taskList(request):
 	tasks = Task.objects.all()
 	serializer = TaskSerializer(tasks, many=True)
-	return render(request,'list.html', {'tas':serializer.data})
+	return render(request, "home.html",{"tasks":serializer.data})
 
-# @api_view(['GET','POST'])
-# def taskDetail(request):
-# 	pk=None
-# 	if request.method=='POST':
-# 		pk = request.POST.get('option', None)
-# 		print(pk)
-# 	if pk is not None:
-# 		task = get_object_or_404(Task, id=pk)
-# 		tasks = Task.objects.get(id=pk)
-# 		serializer = TaskSerializer(tasks, many=False)
-# 	else:
-# 		tasks = Task.objects.all()
-# 		serializer = TaskSerializer(tasks, many=True)
-# 	print(pk)
-# 	return render(request,'list.html', {'det':serializer.data, 'pk': pk})
+
 
 @api_view(['GET', 'POST'])
 def taskDetail(request):
-    if request.method == 'POST':
-        selected_option = request.POST.get('option', None)
-        if selected_option is not None:
-            task = get_object_or_404(Task, id=selected_option)
-            serializer = TaskSerializer(task)
-            return render(request, 'list.html', {'task_detail': Task.objects.all(), 'pk': selected_option, 'task_detail': task})
-        else:
-            return Response({"error": "No option selected."}, status=400)
-    else:
-        tasks = Task.objects.all()
-        serializer = TaskSerializer(tasks, many=True)
-        return render(request, 'list.html', {'task_detail': serializer.data})
+	if request.method == 'GET':
+		tasks = Task.objects.all()
+		serializer = TaskSerializer(tasks, many=True)
+		return render(request, 'home.html', {'detail':serializer.data})
+	if request.method == 'POST':
+		pk = request.data.get('pk')
+		print(type(pk))
+		tasks = Task.objects.get(id=pk)
+		serializer = TaskSerializer(tasks, many=False)
+		return render(request, 'home.html', {'id':tasks.id, 'title':tasks.title, 'status':tasks.completed, 'pk':pk })
 
-@api_view(['POST'])
+
+@api_view(['POST', 'GET'])
 def taskCreate(request):
-	serializer = TaskSerializer(data=request.data)
-	if serializer.is_valid():
-		serializer.save()
-	return Response(serializer.data)
+	is_updat = True
+	if request.method == 'POST':
+		serializer = TaskSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return render(request, 'home.html', {'crea':'Data creation successful', 'is_update': is_updat})
+		else:
+	         return render(request, 'home.html', {'crea':'Data creation unsuccessful', 'is_update': is_updat})
+	if request.method == 'GET':
+	     return render(request, 'home.html', {'is_update': is_updat})
 
-# @api_view(['POST'])
-# def taskUpdate(request, pk):
-# 	task = Task.objects.get(id=pk)
-# 	serializer = TaskSerializer(instance=task, data=request.data)
+@api_view(['POST',"GET"])
+def taskUpdate(request):
+	try:
+		if request.method == 'POST':
+			pk = request.data.get('pk')
+			task = Task.objects.get(id=pk)
+			serializer = TaskSerializer(instance=task, data=request.data)
 
-# 	if serializer.is_valid():
-# 		serializer.save()
+			if serializer.is_valid():
+				serializer.save()
+				return render(request, 'home.html', {'updat':'Data Updation successful'})
+			else:
+				return render(request, 'home.html', {'updat':'Data Updation successful'})
+		if request.method == 'GET':
+			return render(request, 'home.html')
+	except:
+		return render(request, 'home.html',{'updat': 'Please check your entry'})
 
-# 	return Response(serializer.data)
-
-@api_view(['GET', 'POST'])
-def taskUpdate(request, pk):
-    task = get_object_or_404(Task, id=pk)
-
-    if request.method == 'POST':
-        title = request.POST.get('title')
-        description = request.POST.get('description')
-
-        # Update the task with new data
-        task.title = title
-        task.description = description
-        task.save()
-
-    return render(request, 'list.html', {'det': Task.objects.all(), 'pk': None})
 
 
 @api_view(['DELETE'])
@@ -97,20 +82,5 @@ def taskDelete(request, pk):
 	task.delete()
 
 	return Response('Item succsesfully delete!')
-
-
-
-######### Front end  ###########
-
-# @api_view(['GET'])
-# def product_list(request):
-#     products = Task.objects.all()
-#     serializer = TaskSerializer(Task, many=True)
-#     return Response(serializer.data)
-
-
-
-def home(request):
-    return render(request, 'list.html')  # Render the list.html template
 
 
